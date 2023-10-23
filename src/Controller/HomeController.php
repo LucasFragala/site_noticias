f89350e727f3e08cdf00a8ec982cd4fbac5da480
Lucas Fragalá <lucfragala@gmail.com>
2023-10-23 14:56:09 -0300

    up

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/home.html.twig',[
+        return $this->render('home.html.twig',[
+
+        ]);
+    }
+
+    #[Route('/categoria', name: 'app_category')]
+    public function category(): Response
+    {
+        return $this->render('category.html.twig',[
 
         ]);
     }
