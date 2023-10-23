d61af270c081a76840f7fb24c63b29623ef0b4e9
Lucas Fragalá <lucfragala@gmail.com>
2023-10-23 16:37:52 -0300

    up

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsController extends AbstractController
+{
+    #[Route('api/news{id}')]
+    public function getNew(string $id=null): Response
+    {
+        // TODO - criar uma query real
+        $new = [
+            "id" => $id,
+            "título" => "Artista brasileiro é premiado em festival internacional de cinema",
+            "categoria" => "Cultura",
+            "descricao" => "O artista brasileiro João da Silva ganhou o premio de melhor filme de comédia nacional",
+            "data" => "2023-10-12",
+            "imagem" => "https://exemplo.com/imagem/arte.jpg"
+        ];
+        return new JsonResponse($new);
+    }
+
+}
